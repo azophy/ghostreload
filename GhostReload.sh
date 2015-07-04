31df6ec2d2ef24b52bfa1b2971da33eb1f02f4e8
Azophy <azophy@gmail.com>
2015-07-04 08:15:43 +0700

    initial commit, successful basic reload, but the search command use 'tail' instead of 'head'. Still figuring out why the head command fail

---
+++ GhostReload.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Abdurrahman Shofy Adianto <azophy@gmail.com>
+# A modified version of scripts by Silviu Tantos <http://www.razius.com> 
+# and L Nix <lornix@lornix.com>
+#
+# Watches the folder/files passed as arguments to the script and when it
+# detects a change it automatically refreshes the current selected Browser 
+# (also passed as argument) tab or window.
+#
+# Usage:
+# ./GhostReload.sh browser_name /folder/to/watch /some/folder/file_to_watch.html
+
+# whether to use SHIFT+CTRL+R to force reload without cache
+RELOAD_KEYS="CTRL+R"
+#RELOAD_KEYS="SHIFT+CTRL+R"
+#
+
+# set to whatever's given as argument
+BROWSER=$1
+#
+# if was empty, default set to name of browser, firefox/chrome/opera/etc..
+if [ -z "${BROWSER}" ]; then
+    BROWSER=firefox
+fi
+#
+
+TIME_FORMAT='%F %H:%M'
+OUTPUT_FORMAT='%T Event(s): %e fired for file: %w. Refreshing.'
+WATCHED_EVENT="modify"
+
+while inotifywait -e "${WATCHED_EVENT}" -q -r --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "$2"; do
+    #WINDOW_ID=$(xdotool search --onlyvisible --class google-chrome | tail -1)
+    WINDOW_ID=$(xdotool search --onlyvisible --name ${BROWSER} | tail -1)
+    echo "Current Window ID = " $WINDOW_ID
+    xdotool key --window $WINDOW_ID 'CTRL+r'
+done
+
+# auto focus (optional)
+# xdotool windowfocus --sync ${WINDOW_ID}
+# xdotool windowactivate --sync ${WINDOW_ID}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ghostreload
-A short bash script to automatic browser reload on file change
+# GhostReaload
+A short bash script to automate browser reload based on file change
+
+Created by : Abdurrahman Shofy Adianto <azophy@gmail.com>
+This script is a modified version of scripts by Silviu Tantos <http://www.razius.com> and L Nix <lornix@lornix.com>
+
+Watches the folder/files passed as arguments to the script and when it detects a change it automatically refreshes the current selected Browser (also passed as argument) tab or window.
+
+#Requirement
+inotifywait
+xdotools
+
+#installation
+
+install inotify and xdotool first:
+> apt-get install inotify-tools xdotool
+
+then download this script and make it executable:
+> cd ghostreload
+> chmod +x GhostReload.sh
+
+#Usage:
+> ./GhostReload.sh browser_name /folder/to/watch /some/folder/file_to_watch.html
