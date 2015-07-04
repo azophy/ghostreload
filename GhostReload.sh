7e7fb9fb7607beec6dc5b4b36acad268768aa326
Azophy <azophy@gmail.com>
2015-07-05 06:02:48 +0700

    added functionality to add more then 1 target to watch

--- GhostReload.sh
+++ GhostReload.sh
@@ -14,26 +14,26 @@
 # whether to use SHIFT+CTRL+R to force reload without cache
 RELOAD_KEYS="CTRL+R"
 #RELOAD_KEYS="SHIFT+CTRL+R"
-#
 
 # set to whatever's given as argument
 BROWSER=$1
-#
+
 # if was empty, default set to name of browser, firefox/chrome/opera/etc..
 if [ -z "${BROWSER}" ]; then
     BROWSER=firefox
 fi
-#
 
+args=("$@")
+WATCHED_FOLDERS=(${args[@]:1})
 TIME_FORMAT='%F %H:%M'
 OUTPUT_FORMAT='%T Event(s): %e fired for file: %w. Refreshing.'
 WATCHED_EVENT="modify"
 
-while inotifywait -e "${WATCHED_EVENT}" -q -r --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "$2"; do
+while inotifywait -e "${WATCHED_EVENT}" -q -r --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "${WATCHED_FOLDERS[@]}"; do
     #WINDOW_ID=$(xdotool search --onlyvisible --class google-chrome | tail -1)
     WINDOW_ID=$(xdotool search --onlyvisible --name ${BROWSER} | tail -1)
     echo "Current Window ID = " $WINDOW_ID
-    xdotool key --window $WINDOW_ID 'CTRL+r'
+    xdotool key --window $WINDOW_ID $RELOAD_KEYS
 done
 
 # auto focus (optional)
